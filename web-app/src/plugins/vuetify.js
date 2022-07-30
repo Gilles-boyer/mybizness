969fb1f31dcf57f5adef6c35361c1ed35df47e45
Gilles BOYER <boyer.gilles@live.fr>
2022-07-30 15:43:24 +0400

    init web app vue

---
+++ web-app/src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  light: {
+    primary: "#04153B",
+    secondary: "#A09F9F",
+    accent: "#FD801F",
+    error: "#FE001A",
+  },
+});
